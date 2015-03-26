808a12e52a05f4f379bc496a9257ff858626babd
stevepeak <steve@stevepeak.net>
2015-03-26 08:26:46 +0800

    v++

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### `0.0.5`
+- added line messages by @coderanger
+- fixed skip lines during reporting by @coderanger
+
 ### `0.0.4`
 - added more test
 - added more CI providers
--- lib/codecov.rb
+++ lib/codecov.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'net/http'
 
 class SimpleCov::Formatter::Codecov
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
   def format(result)
     # =================
     # Build JSON Report
