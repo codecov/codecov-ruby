9acb06c799bfe139c38f48285ea24628ecbf2596
Steve Peak <steve@stevepeak.net>
2016-02-25 08:04:35 +1300

    store toplvl for quicker use

--- lib/codecov.rb
+++ lib/codecov.rb
@@ -268,7 +268,10 @@ class SimpleCov::Formatter::Codecov
   # @param file [SimeplCov::SourceFile] The file to use.
   # @return [String]
   def shortened_filename(file)
-    file.filename.gsub(/^#{`git rev-parse --show-toplevel`.strip!}/, '.').gsub(/^\.\//, '')
+    if defined?(@toplvl)
+      @toplvl = `git rev-parse --show-toplevel`.strip
+    end
+    file.filename.gsub(/^#{@toplvl}/, '.').gsub(/^\.\//, '')
   end
 
 
