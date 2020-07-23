c247cce44e05ae0cd77f9a1ab9b31a21ace4c2df
SAKATA Sinji <ysakasin@gmail.com>
2020-07-23 21:05:50 +0900

    Add support GitHub Actions
    Based on https://github.com/codecov/codecov-ruby/pull/52
    
    Co-authored-by: Timo Schilling <timo@schilling.io>

--- test/test_codecov.rb
+++ test/test_codecov.rb
@@ -150,6 +150,12 @@ class TestCodecov < Minitest::Test
     ENV['ghprbSourceBranch'] = nil
     ENV['GIT_BRANCH'] = nil
     ENV['GIT_COMMIT'] = nil
+    ENV['GITHUB_ACTIONS'] = nil
+    ENV['GITHUB_REF'] = nil
+    ENV['GITHUB_HEAD_REF'] = nil
+    ENV['GITHUB_REPOSITORY'] = nil
+    ENV['GITHUB_RUN_ID'] = nil
+    ENV['GITHUB_SHA'] = nil
     ENV['GITLAB_CI'] = nil
     ENV['HEROKU_TEST_RUN_ID'] = nil
     ENV['HEROKU_TEST_RUN_BRANCH'] = nil
@@ -397,6 +403,22 @@ class TestCodecov < Minitest::Test
     assert_equal('1', result['params'][:build])
     assert_equal('master', result['params'][:branch])
     assert_equal('owner/repo', result['params'][:slug])
+    assert_equal('f881216b-b5c0-4eb1-8f21-b51887d1d506', result['params']['token'])
+  end
+
+  def test_github
+    ENV['CI'] = 'true'
+    ENV['GITHUB_ACTIONS'] = 'true'
+    ENV['GITHUB_REF'] = 'refs/head/master'
+    ENV['GITHUB_REPOSITORY'] = 'codecov/ci-repo'
+    ENV['GITHUB_RUN_ID'] = '1'
+    ENV['GITHUB_SHA'] = 'c739768fcac68144a3a6d82305b9c4106934d31a'
+    ENV['CODECOV_TOKEN'] = 'f881216b-b5c0-4eb1-8f21-b51887d1d506'
+    result = upload
+    assert_equal('github-actions', result['params'][:service])
+    assert_equal('c739768fcac68144a3a6d82305b9c4106934d31a', result['params'][:commit])
+    assert_equal('codecov/ci-repo', result['params'][:slug])
+    assert_equal('1', result['params'][:build])
     assert_equal('f881216b-b5c0-4eb1-8f21-b51887d1d506', result['params']['token'])
   end
 
