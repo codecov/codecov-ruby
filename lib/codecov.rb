f677699f0694d0235b6e56c77b534e2b52e15b4f
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-11-26 18:00:07 -0300

    Fix branch name detection for GitHub Actions CI (#111)
    * Fix typo (it is 'refs/heads/' not 'refs/head')
    
    * Detect GITHUB_HEAD_REF='' as falsey

--- lib/codecov.rb
+++ lib/codecov.rb
@@ -192,9 +192,13 @@ class SimpleCov::Formatter::Codecov
     when GITHUB
       # https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables
       params[:service] = 'github-actions'
-      params[:branch] = ENV['GITHUB_HEAD_REF'] || ENV['GITHUB_REF'].sub('refs/head/', '')
-      # PR refs are in the format: refs/pull/7/merge for pull_request events
-      params[:pr] = ENV['GITHUB_REF'].split('/')[2] unless ENV['GITHUB_HEAD_REF'].nil? || ENV['GITHUB_HEAD_REF'].empty?
+      if (ENV['GITHUB_HEAD_REF'] || '').empty?
+        params[:branch] = ENV['GITHUB_REF'].sub('refs/heads/', '')
+      else
+        params[:branch] = ENV['GITHUB_HEAD_REF']
+        # PR refs are in the format: refs/pull/7/merge for pull_request events
+        params[:pr] = ENV['GITHUB_REF'].split('/')[2]
+      end
       params[:slug] = ENV['GITHUB_REPOSITORY']
       params[:build] = ENV['GITHUB_RUN_ID']
       params[:commit] = ENV['GITHUB_SHA']
